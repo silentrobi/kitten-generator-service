378f76bc2f797d0f9050ca2bf7584c6f1b86aa08
Mohammad Abu Musa Rabiul <mohammad.rabiul@tokeninc.com>
2021-12-18 16:02:10 +0300

    bug fix

--- KittenGeneratorService.Api/Controllers/UserController.cs
+++ KittenGeneratorService.Api/Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using KittenGeneratorService.Application.Commands;
+﻿using KittenGeneratorService.Api.Requests;
+using KittenGeneratorService.Application.Commands;
 using KittenGeneratorService.Application.Queries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace KittenGeneratorService.Api.Controllers
@@ -16,7 +18,7 @@ namespace KittenGeneratorService.Api.Controllers
         /// <param name="request"></param>
         /// <returns></returns>
         [HttpPost]
-        public async Task<IActionResult> Create(CreateUser request)
+        public async Task<IActionResult> Create([FromBody] CreateUser request)
         {
             var result = await Mediator.Send(request);
 
@@ -29,6 +31,30 @@ namespace KittenGeneratorService.Api.Controllers
             var result = await Mediator.Send(new GetUsers());
 
             return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateUserRequest updateUserRequest)
+        {
+            var updateUser = new UpdateUser()
+            {
+                Id = id,
+                Password = updateUserRequest.Password,
+                Role = updateUserRequest.Role,
+                Username = updateUserRequest.Username
+            };
+
+            var result = await Mediator.Send(updateUser);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+           await Mediator.Send(new DeleteUser() { Id= id});
+
+            return Ok();
         }
     }
 }
---
+++ KittenGeneratorService.Api/Requests/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using KittenGeneratorService.Domain.ValueObjects;
+
+namespace KittenGeneratorService.Api.Requests
+{
+    public class UpdateUserRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
